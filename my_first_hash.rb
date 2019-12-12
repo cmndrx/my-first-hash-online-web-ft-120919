b2f3b1f4faed8d76475d88f89436a439dcea0cd8
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-12 22:16:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,14 +26,6 @@ def adding
                        "muskets" => 2,
                        "gun powder" => 4
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  
   shipping_manifest
 end
